eb7f4d6d98a924973195356bcd441531b07c8f89
William Velarde <williamvelarde@Williams-MacBook-Air.local>
2016-12-15 14:18:20 -0700

    added some styling

--- game.js
+++ game.js
@@ -49,7 +49,7 @@ var mainState = {
 		createEnemies();
 
 		scoreText = game.add.text(0, 550, 'Score', {font: '32px Impact', fill: '#fff'});
-		winText = game.add.text(game.world.centerX - 15, game.world.centerY, 'You Win!', {font: '32px Impact', fill: '#fff'});
+		winText = game.add.text(game.world.centerX - 30, game.world.centerY, 'You Win!', {font: '32px Impact', fill: '#fff'});
 		winText.visible = false;
 
 	},
@@ -97,15 +97,15 @@ function fireBullet() {
 function createEnemies(){
 	for(var y = 0;y < 4;y++){
 		for(var x = 0;x < 10;x++){
-			var enemy = enemies.create(x * 48, y * 50, 'enemy');
+			var enemy = enemies.create(x * 60, y * 50, 'enemy');
 			enemy.anchor.setTo(0.5, 0.5);
 		}
 	}
 
-enemies.x = 100;
+enemies.x = 50;
 enemies.y = 50;
 
-var tween = game.add.tween(enemies).to({x:200}, 2000,Phaser.Easing.Linear.None,true,0,1000,true);
+var tween = game.add.tween(enemies).to({x:200}, 1000,Phaser.Easing.Linear.None,true,0,1000,true);
 
 tween.onLoop.add(descend, this);
 }
--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
   var $orders = $('#orders');
-  var $name = $('#name');
-
+  var $name = $('#first_name2');
+// console.log($name)
 function getBoard(){
   $.ajax({
     type: 'GET',
-    url: 'https://galvanize-leader-board.herokuapp.com/api/v1/leader-board/Deadpool',
+    url: 'https://galvanize-leader-board.herokuapp.com/api/v1/leader-board/Space',
     success: function(orders) {
       $.each(orders, function(i, order) {
-        $orders.append('<li>name: '+ order.player_name +', score: '+ order.score + '</li>');
+        $orders.append('<li>'+ order.player_name + order.score + '</li>');
       });
     },
     error: function() {
@@ -22,11 +22,11 @@ function getBoard(){
     event.preventDefault();
 
     var order = {
-      game_name: "Deadpool",
-      player_name: "William",
-      score: 90
+      game_name: "Space",
+      player_name: $name,
+      score: score
     };
-
+// console.log($name);
     $.ajax({
       type: "POST",
       url: "https://galvanize-leader-board.herokuapp.com/api/v1/leader-board",
